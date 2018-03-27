4cc4d52e6c9d13c1b2f956aa294131e12a4d7f5d
DimidroL <kuznecov_da@inbox.ru>
2018-03-27 13:58:39 +0500

    Fix slug.js

--- slug.js
+++ slug.js
@@ -3,14 +3,14 @@
  */
 
 function generateSlug(title, arrItem = 0) {
-    var tranlit = 'a b v g d e ["zh","j"] z i y k l m n o p r s t u f h ts ch sh ["shh","shch"] ~ y ~ e yu ya ~ ["jo","e"]'.split(' ');
+    var translit = 'a b v g d e ["zh","j"] z i y k l m n o p r s t u f h ts ch sh ["shh","shch"] ~ y ~ e yu ya ~ ["jo","e"]'.split(' ');
     var exceptionChars = '№!@#$%^&*();:?*';
     var slug = '';
     title = title.toLowerCase();
     for (i = 0; i < title.length; ++i) {
 	if (exceptionChars.indexOf(title.charAt(i)) < 0) {
 	    code = title.charCodeAt(i);
-	    charOfSlug = (code >= 1072 ? tranlit[code - 1072] : title[i]);
+	    charOfSlug = (code >= 1072 ? translit[code - 1072] : title[i]);
 	    slug += charOfSlug.length > 2 ? eval(charOfSlug)[arrItem] : charOfSlug;
 	};
     }
